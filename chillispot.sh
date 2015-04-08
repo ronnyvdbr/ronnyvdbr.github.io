4909baae1a029c509edae695ff9f9f11060d2689
Ronny Van den Broeck <ronny.vandenbroeck@telenet.be>
2015-04-08 23:13:18 +0200

    Update chillispot.sh

--- chillispot.sh
+++ chillispot.sh
@@ -47,6 +47,7 @@ echo "iptables –I POSTROUTING –t nat –o $HS_WANIF –j MASQUERADE" | tee -
 sed -i 's/haserl=$(which haserl 2>\/dev\/null)/haserl=\/usr\/local\/bin\/haserl/g' /etc/chilli/wwwsh
 sudo sed -i 's/# Default-Start:  2 3 5/# Default-Start:  2 3 4 5/g' /etc/init.d/chilli
 sudo sed -i 's/# Default-Stop:/# Default-Stop:  0 1 6/g' /etc/init.d/chilli
+reboot
 
 
 
