cf4feae6d7fcb2ffb2ac071dbd75fbcb8a3254b2
Ronny Van den Broeck <ronny.vandenbroeck@telenet.be>
2015-05-23 08:17:52 +0200

    Update login_database.sh

--- login_database.sh
+++ login_database.sh
@@ -7,6 +7,15 @@ CREATE TABLE users ( \
   PRIMARY KEY  (id) \
 ) ;" | mysql --host=localhost --user=root --password=raspberry --database login
 
+CREATE TABLE openvpnusers ( \
+  id int(11) NOT NULL auto_increment, \
+  username varchar(64) NOT NULL default '', \
+  firstname varchar(64) NOT NULL default '', \
+  lastname varchar(64) NOT NULL default '', \
+  packageurl varchar(64) NOT NULL default '', \
+  PRIMARY KEY  (id) \
+) ;" | mysql --host=localhost --user=root --password=raspberry --database login
+
 echo "INSERT INTO users (username,password) VALUES('admin','raspberry');" | \
 mysql --host=localhost --user=root --password=raspberry --database login
 
