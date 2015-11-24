52009fcd0a1e22bcf473d5dfa78387dc06307afd
Ronny Van den Broeck <ronny.vandenbroeck@telenet.be>
2015-11-24 14:11:40 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,6 +5,7 @@ dpkg-reconfigure -f noninteractive tzdata
 sed -i "s/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
 /usr/sbin/locale-gen
 apt-get -y install sudo
+sudo apt-get -y install firmware-ralink
 sudo apt-get -y install lighttpd php5-common php5-cgi php5
 sudo lighty-enable-mod fastcgi-php
 sudo rm -R /var/www
