59ccd351b1c47ace87812caf439590e9c85b342f
Ronny Van den Broeck <ronny.vandenbroeck@telenet.be>
2015-11-25 19:56:22 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -196,7 +196,5 @@ mysql --host=localhost --user=root --password=raspberry --database login
 ########################################################################################
 apt-get -y install openvpn
 apt-get -y install zip
-mkdir /etc/openvpn/easy-rsa
-cp -R /usr/share/doc/openvpn/examples/easy-rsa/2.0/* /etc/openvpn/easy-rsa
 mkdir /var/www/temp/OpenVPN_ClientPackages
 update-rc.d -f openvpn remove
