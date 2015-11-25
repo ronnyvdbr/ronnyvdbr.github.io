180aca32fa14a3e8779d1ae999831643513a2993
Ronny Van den Broeck <ronny.vandenbroeck@telenet.be>
2015-11-25 16:40:10 +0100

    Delete openvpn.sh

--- openvpn.sh
+++
@@ -1,7 +0,0 @@
-apt-get -y install openvpn
-apt-get -y install zip
-mkdir /etc/openvpn/easy-rsa
-cp -R /usr/share/doc/openvpn/examples/easy-rsa/2.0/* /etc/openvpn/easy-rsa
-mkdir /var/www/temp/OpenVPN_ClientPackages
-update-rc.d -f openvpn remove
-
